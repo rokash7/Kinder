c4535e0cc6bd752994be179690a5ac8b0b7d71e4
Olegas Borisovas <90721784+Olegas123@users.noreply.github.com>
2021-09-21 22:43:49 +0300

    New User.cs class template

---
+++ Kinder/Classes/User.cs
@@ -0,0 +1,15 @@
+using System;
+public class User
+{
+    public string Name { get; set;}
+    public string SurName { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public int Id { get; set; }
+    public int KarmaPoints { get; set; }
+
+
+    public User() {}
+
+}
