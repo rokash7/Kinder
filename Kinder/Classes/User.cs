02069a6a3dd74aa4d87dca796c4750fa3a18bc9c
Olegas Borisovas <90721784+Olegas123@users.noreply.github.com>
2021-09-22 09:18:29 +0300

    User.cs updated

--- Kinder/Classes/User.cs
+++ Kinder/Classes/User.cs
@@ -2,11 +2,12 @@ using System;
 public class User
 {
     public string Name { get; set;}
-    public string SurName { get; set; }
+    public string Surname { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
-    public int Id { get; set; }
+    public int Id { get; }
+    public int Password { get; set; }
     public int KarmaPoints { get; set; }
 
 
