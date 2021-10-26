c01ac81ce8560a25dcbdc8055adda324c1801a6f
Konstantinas Bernotas <kostismin3craft@gmail.com>
2021-10-27 02:37:47 +0300

    Moved files to seperate folder

--- Kinder/Database/Users.cs
+++ Kinder/Database/Users.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace Kinder
+namespace Kinder.Database
 {
     public class Users
     {
--- Kinder/Database/UsersContext.cs
+++ Kinder/Database/UsersContext.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace Kinder
+namespace Kinder.Database
 {
     class UsersContext : DbContext
     {
