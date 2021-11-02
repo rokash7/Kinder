8e3e49c17338a2a211808221b08d8b873a94163b
Konstantinas Bernotas <kostismin3craft@gmail.com>
2021-11-02 23:00:47 +0200

    Added Items table

--- Kinder/Database/UsersContext.cs
+++ Kinder/Database/UsersContext.cs
@@ -10,6 +10,7 @@ namespace Kinder.Database
     class UsersContext : DbContext
     {
         public DbSet<Users> Users { get; set; }
+        public DbSet<Items> Items { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Kinder; Trusted_Connection=True;");
